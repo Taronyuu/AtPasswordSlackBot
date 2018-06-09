af2c0ac7e0399e8b360349d9dc161f98a7b58ef2
Your Name <you@example.com>
2018-06-09 07:35:13 +0000

    Tried to fix this

--- src/main/java/nl/zandervdm/AtPassword/Main.java
+++ src/main/java/nl/zandervdm/AtPassword/Main.java
@@ -1,9 +1,22 @@
 package nl.zandervdm.AtPassword;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "example.jbot"})
 public class Main {
+	
+	@Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    } 
 
     public static void main(String[] args) {
+		System.out.println("[AtPassword] Starting application");
 
+		SpringApplication.run(Main.class, args);
     }
 
 }
