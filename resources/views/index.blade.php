9fb4fc8cbe57eab14f55d67937678714ef3b3ffc
Zander van der Meer <zander.meer@gmail.com>
2018-06-17 19:22:19 +0200

    Added more slack buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
         <span class="logo"><img src="{{ url('images/logo-small.png') }}" alt="" /></span>
         <h1>At Password</h1>
         <p>A Slack app to securely share passwords with your teammates.</p>
+        <p class="no-bottom">@include('master.partials.slack')</p>
     </header>
 
     <!-- Nav -->
@@ -90,7 +91,7 @@
             <footer class="major">
                 <ul class="actions special">
                     <li>
-                        <a href="https://slack.com/oauth/authorize?client_id=298768238515.378589373155&scope=commands"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+                        @include('master.partials.slack')
                     </li>
                     {{--<li><a href="generic.html" class="button">Learn More</a></li>--}}
                 </ul>
---
+++ resources/views/master/partials/slack.blade.php
@@ -0,0 +1,1 @@
+<a class="slack" href="https://slack.com/oauth/authorize?client_id=298768238515.378589373155&scope=commands"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
